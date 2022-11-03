2e626e417180bb718ec038c3736400dd829fce83
vishal-Tx <vishalguptaa2z111@gmail.com>
2022-11-03 19:41:01 +0530

    minor update

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import ExpenseData from "../../ExpenseData";
+import Card from "../UI/Card";
+import "./Expenses.css";
 
-const Expenses = (props) => {
+const Expenses = () => {
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {ExpenseData.map((expenseData) => {
+        return (
+          <ExpenseItem
+            key={expenseData.id}
+            title={expenseData.title}
+            date={expenseData.date}
+            amount={expenseData.amount}
+          />
+        );
+      })}
     </Card>
   );
-}
+};
 
 export default Expenses;
